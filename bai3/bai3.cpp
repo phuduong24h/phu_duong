7108993370d2b3a35e442ce66947d19adf9f1490
phuduong24h <phuduong24h@gmail.com>
2023-01-27 16:56:45 +0700

    abc

--- bai3/bai3.cpp
+++ bai3/bai3.cpp
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
-int main()       {
-    int a,c, b;
-   c=a+b;
-   printf("hay nhap a va b");
-   scanf("%d %d", &a, &b);
- c=a+b;#include <stdio.h>
+int main()
+{
+    int a, c, b;
 
-int main()       {
-    int a,c, b;
-   c=a+b;
-   printf("hay nhap a va b");
-   scanf("%d %d", &a, &b);
- c=a+b;
-    printf("hay nhap a va %d",c);
-    return 0;
-}
-    printf("hay nhap a va %d",c);
+    c = a + b;
+    printf("hay nhap a va b");
+    scanf("%d %d", &a, &b);
+    c = a + b;
+#include <stdio.h>
+
+int main()
+    {
+        int a, c, b;
+        c = a + b;
+        printf("hay nhap a va b");
+        scanf("%d %d", &a, &b);
+        c = a + b;
+        printf("hay nhap a va %d", c);
+        return 0;
+    }
+    printf("hay nhap a va %d", c);
     return 0;
 }
